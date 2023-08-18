d5a7e211d1e5ad942196d83450553e6eb6eb9669
shanilka-fernando <shanilkaf.creativehub@gmail.com>
2023-08-18 15:32:54 +0530

    ui : user profile screen

--- src/activities/profile/profileScreen.tsx
+++ src/activities/profile/profileScreen.tsx
@@ -1,9 +1,17 @@
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar} from 'react-native';
+import theme from '../../theme/theme';
+import {AndroidSafeArea} from '../../styles/globalStyles';
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={AndroidSafeArea}>
+      <StatusBar translucent backgroundColor="transparent" />
+      
+      <View style={[{backgroundColor:theme.colors.primary.primary300}]}>
+        <Text style={[{backgroundColor: theme.colors.gray.gray200}]}>
+          ProfileScreen
+        </Text>
+      </View>
     </View>
   );
 };
